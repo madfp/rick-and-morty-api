d6a732c3f3f3304c60bf735b4fa7318ce448999d
Marco De Freitas <marcoantoniodfp@gmail.com>
2023-11-17 13:41:39 -0400

    Add: Checking and rendering status

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -3,8 +3,6 @@ import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
 import { CharacterData } from '../../utils/models';
@@ -41,27 +39,30 @@ export default function AlertDialogSlide({character}:{character:CharacterData}) 
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle className='text-lg font-bold'>{character.name}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description" className='flex gap-2'>
-            <div className='w-[50%] flex flex-col items-center justify-center'>
+        <DialogContent className='flex flex-col sm:flex-row gap-2'>
+            <div className='w-full lg:w-[50%] flex flex-col items-center justify-center'>
                 <img src={character.image} alt="Character image" />
             </div>
-            <div className='w-[50%] mx-l-2'>
+            <div className='w-full lg:w-[50%] mx-l-2'>
                 <h2 className='text-lg font-bold text-center'>Character info</h2>
                 <div>
+                    <span className="opacity-[.7]">Name:</span>
+                    <h3 className='font-bold'>{character.name}</h3>
+                </div>
+                <div>
                     <span className="opacity-[.7]">Status:</span>
-                    <h3>{character.status}</h3>
+                    <h3 className='font-bold'>{
+                        character.status==="Alive" ? "🟢" : character.status==="Dead" ? "🔴" : "🟡"
+                    }{character.status}</h3>
                 </div>
                 <div>
                     <span className="opacity-[.7]">Species:</span>
-                    <h3>{character.species}</h3>
-                </div>
+                    <h3 className='font-bold'>{character.species}</h3>
+                </div>  
             </div>
-          </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
+          <Button onClick={handleClose} variant='contained'>Close</Button>
         </DialogActions>
       </Dialog>
     </Fragment>
